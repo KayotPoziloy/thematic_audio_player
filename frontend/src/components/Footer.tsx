06a92ebb4c3103c97e7c4f491cf99109bd5c39ca
Айдар <farhutdinov04@gmail.com>
2024-10-12 13:36:09 +0300

    add Footer page

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer  className="bg-dark text-white d-flex align-items-center justify-content-center">
+            footer
+        </footer>
+    );
+}
