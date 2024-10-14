2b2ca579339c1dc32246fd6b5bf452b27f38795d
Maksim <78742120+KayotPoziloy@users.noreply.github.com>
2024-10-14 10:45:50 +0300

    Revert "add Footer page"
    This reverts commit 2b0f54f0d9105ad19d2b04c0f140af5f1e04fa93.

--- frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <footer  className="bg-dark text-white d-flex align-items-center justify-content-center">
-            footer
-        </footer>
-    );
-}
