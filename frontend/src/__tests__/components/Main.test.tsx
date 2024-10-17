d00de1e4a80e7a964478de948b89a98e6fae816f
et-yula <et.yula25@gmail.com>
2024-10-17 17:39:56 +0300

    G3.1: up coverage

--- frontend/src/__tests__/components/Main.test.tsx
+++ frontend/src/__tests__/components/Main.test.tsx
@@ -4,32 +4,60 @@ import Main from '../../components/Main';
 import { text } from 'stream/consumers';
 import userEvent from '@testing-library/user-event'
 
+
 describe("/components/Main.test.tsx", () => {
+  
+
   ['Start', 'Stop'].forEach(text => {
-    test('text "' + text+ '" not found', () => {
+    test('text "' + text + '" not found', () => {
       render(<Main />);
       const element = screen.getByText(new RegExp(text, 'i'));
       expect(element).toBeInTheDocument();
     });
   });
 
+  
   test('Stop', () => {
     const mockPause = jest
       .spyOn(window.HTMLMediaElement.prototype, 'pause')
-      .mockImplementation(() => {});
+      .mockImplementation(() => { });
+    
     render(<Main />);
-    userEvent.click(screen.getByText('Stop'));
-    expect(mockPause).toHaveBeenCalled();
-  });
 
-  test('Start', () => {
+    userEvent.click(screen.getByText('Stop')); 
+    expect(mockPause).toHaveBeenCalled();
+    
+    
+  });
+  
+  test('Start catch', () => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        json: () => Promise.resolve(''),
+        ok: false,
       })
-    ) as jest.Mock;
+    );
     render(<Main />);
     userEvent.click(screen.getByText('Start'));
     expect(global.fetch).toHaveBeenCalled();
   });
+
+  test('Start try',  () => {
+    const mockPlay = jest
+      .spyOn(window.HTMLMediaElement.prototype, 'play')
+      .mockImplementation(() => { });
+    const mockAudioBlob = new Blob(['audio data'], { type: 'audio/mpeg' });
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        blob: jest.fn().mockResolvedValueOnce(mockAudioBlob),
+      })
+    );
+    global.URL.createObjectURL = jest.fn().mockReturnValue('mocked-audio-url');
+
+    render(<Main />); 
+    userEvent.click(screen.getByText('Start'));
+    expect(global.fetch).toHaveBeenCalledWith("http://localhost:5000/audio");
+  }); 
+
 });
