312b9630eff148c695f18b0425b2499ccc08834b
et-yula <et.yula25@gmail.com>
2024-10-17 21:46:56 +0300

    G3.5 added unit broken test

--- frontend/src/__tests__/components/Main.test.tsx
+++ frontend/src/__tests__/components/Main.test.tsx
@@ -40,20 +40,5 @@ describe("/components/Main.test.tsx", () => {
     expect(global.fetch).toHaveBeenCalled();
   });
 
-  test('Start try',  () => {
-    const mockAudioBlob = new Blob(['audio data'], { type: 'audio/mpeg' });
-
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        blob: jest.fn().mockResolvedValueOnce(mockAudioBlob),
-      })
-    );
-    global.URL.createObjectURL = jest.fn().mockReturnValue('mocked-audio-url');
-
-    render(<Main />); 
-    userEvent.click(screen.getByText('Start'));
-    expect(global.fetch).toHaveBeenCalledWith("http://localhost:5000/audio");
-  }); 
 
 });
