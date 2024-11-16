a56a04f0d85e38c8d390044aa4f1d2175c72f125
Илия Илиев <iliailiev@Ilias-MacBook-Air.local>
2024-11-16 13:36:13 +0300

    добавлены интерактивные иконки в настройках пользователя

--- frontend/src/components/Acc_components/Settings.tsx
+++ frontend/src/components/Acc_components/Settings.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
-import "../../style_lk/Account.css";
+import "../../style_lk/Settings.css";
 import UserHeader from "./UserHeader";
+import avatarIcon from ".././Acc_components/img/Settings/img.png";
+import backgroundIcon from ".././Acc_components/img/Settings/img_1.png";
+import editIcon from ".././Acc_components/img/Settings/img_2.png";
+
+// Остальной код...
+
+
 
 export default function Settings() {
     const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
@@ -36,8 +43,9 @@ export default function Settings() {
             <div className="settings-page">
                 <div className="settings-options">
                     <div className="option">
-                        <label className="btn btn-outline-primary">
-                            Добавить фото пользователя
+                        <label className="settings-button">
+                            <img src={avatarIcon} alt="Добавить фото пользователя" className="settings-icon" />
+                            <span>ДОБАВИТЬ АВАТАРКУ</span>
                             <input
                                 type="file"
                                 accept="image/*"
@@ -47,8 +55,9 @@ export default function Settings() {
                         </label>
                     </div>
                     <div className="option">
-                        <label className="btn btn-outline-secondary">
-                            Добавить шапку пользователя
+                        <label className="settings-button">
+                            <img src={backgroundIcon} alt="Добавить шапку пользователя" className="settings-icon" />
+                            <span>ДОБАВИТЬ ШАПКУ</span>
                             <input
                                 type="file"
                                 accept="image/*"
@@ -58,8 +67,9 @@ export default function Settings() {
                         </label>
                     </div>
                     <div className="option">
-                        <button className="btn btn-outline-success">
-                            Редактировать данные
+                        <button className="settings-button">
+                            <img src={editIcon} alt="Редактировать данные" className="settings-icon" />
+                            <span>РЕДАКТИРОВАТЬ ДАННЫЕ</span>
                         </button>
                     </div>
                 </div>
