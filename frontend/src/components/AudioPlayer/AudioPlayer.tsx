dcc4131ce6dffb4dc440f5f968f825914d8ba569
Максим <yakhont04@mail.ru>
2024-11-19 16:12:10 +0300

    clear AudioPlayer.tsx | add track duration

--- frontend/src/components/AudioPlayer/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,132 +1,22 @@
-import React, {useEffect, useRef, useState} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {AppDispatch, RootState} from "../../store/store"
-import {
-    fetchPlaylistTracks,
-    playTrack,
-    pauseTrack,
-    resumeTrack,
-    nextTrack,
-    previousTrack,
-} from "../../store/audioSlice";
+import React, {useState} from "react";
 import "./AudioPlayer.scss"
-import { saveAudioState } from "../../utils/localStorage";
+import {useAudioPlayer} from "../../hooks/useAudioPlayer";
 
 export const AudioPlayer = () => {
-    const audio = useRef<HTMLAudioElement | null>(null);
-    const dispatch = useDispatch<AppDispatch>();
-    const [volume, setVolume] = useState(0.5);
-    const [show, setShow] = useState(false);
-    const {selectedPlaylistId} = useSelector((state: RootState) => state.playlist);
-
     const {
+        audio,
         tracks,
         currentTrackIndex,
         isPlaying,
         currentTime,
-    } = useSelector(
-        (state: RootState) => state.audio
-    );
+        volume,
+        handlePlayPause,
+        handleNext,
+        handlePrevious,
+        handleVolumeChange,
+    } = useAudioPlayer();
 
-    useEffect(() => {
-        if (selectedPlaylistId) {
-            dispatch(fetchPlaylistTracks(selectedPlaylistId));
-        }
-    }, [selectedPlaylistId, dispatch]);
-
-    useEffect(() => {
-        if (audio.current) {
-            const handleTrackEnd = () => {
-                dispatch(nextTrack());
-
-                setTimeout(() => {
-                    if (audio.current) {
-                        try {
-                            audio.current.play();
-                        } catch (error) {
-                            console.error("Error playing next track:", error);
-                        }
-                    }
-                }, 0);
-            };
-
-            audio.current.addEventListener("ended", handleTrackEnd);
-
-            return () => {
-                audio.current?.removeEventListener("ended", handleTrackEnd);
-            };
-        }
-    }, [dispatch]);
-
-    useEffect(() => {
-        if (tracks.length > 0) {
-            const currentTrack = tracks[currentTrackIndex];
-
-            if (audio.current) {
-                audio.current.src = `http://localhost:5000/api/music/m/${currentTrack.filename}`;
-                audio.current.currentTime = currentTime;
-
-                if (isPlaying) {
-                    const playAudio = async () => {
-                        try {
-                            await audio.current?.play();
-                        } catch (error) {
-                            console.error("Error playing audio:", error);
-                        }
-                    };
-
-                    audio.current.addEventListener("loadeddata", playAudio);
-
-                    return () => {
-                        audio.current?.removeEventListener("loadeddata", playAudio);
-                    };
-                } else {
-                    audio.current.pause();
-                }
-            }
-        }
-    }, [tracks, currentTrackIndex, isPlaying, currentTime]);
-
-    useEffect(() => {
-        saveAudioState({
-            currentTrackIndex,
-            currentTime,
-            isPlaying,
-        });
-    }, [currentTrackIndex, currentTime, isPlaying]);
-
-    const handlePlayPause = () => {
-        if (!isPlaying && currentTime === 0) {
-            dispatch(playTrack(currentTrackIndex));
-        } else if (isPlaying) {
-            if (audio.current) {
-                const currentTime = audio.current.currentTime;
-                audio.current.pause();
-                dispatch(pauseTrack(currentTime));
-            }
-        } else {
-            if (audio.current) {
-                audio.current.play();
-                dispatch(resumeTrack());
-            }
-        }
-    };
-
-    const handleNext = () => {
-        dispatch(nextTrack());
-    };
-
-    const handlePrevious = () => {
-        dispatch(previousTrack());
-    };
-
-    const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newVolume = parseFloat(e.target.value);
-        setVolume(newVolume);
-        if (audio.current) {
-            audio.current.volume = newVolume;
-        }
-    }
+    const [show, setShow] = useState(false);
 
     return (
         <div className="footer-player">
