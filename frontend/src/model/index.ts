6182251470e9fc4ae2017abc8e02827bb5ce02cb
Максим <yakhont04@mail.ru>
2024-12-24 03:21:33 +0300

    Moved the function for signout in Model layer

--- frontend/src/model/index.ts
+++ frontend/src/model/index.ts
@@ -2,5 +2,12 @@ import { fetchPlaylistTracks } from "./getMusics";
 import { fetchPlaylists } from "./getPlaylists";
 import { signup } from "./signup";
 import { signin } from "./signin";
+import { useHandleLogout } from "./logout"
 
-export { fetchPlaylistTracks, fetchPlaylists, signup, signin };
+export { 
+    fetchPlaylistTracks, 
+    fetchPlaylists, 
+    signup, 
+    signin, 
+    useHandleLogout 
+};
