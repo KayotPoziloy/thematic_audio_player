b7c52a1009bb9cd855ec81d5329cac2a52cc3702
Илия Илиев <iliailiev@Ilias-MacBook-Air.local>
2025-01-12 01:18:59 +0300

    update user.header

--- frontend/src/components/Acc_components/UserHeader.tsx
+++ frontend/src/components/Acc_components/UserHeader.tsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../style_lk/Account.css";
-import { useSelector } from "react-redux";
-import { RootState } from "../../redux/reducers";
+import axios from "axios";
 
 type UserHeaderProps = {
     backgroundImage?: string | null;
     avatarImage?: string | null;
 };
 
+type UserData = {
+    name: string;
+    login: string;
+};
+
+// Функция для получения данных пользователя с сервера
+export async function fetchUserData() {
+    try {
+        const response = await axios.get("http://localhost:4000/api/user/info", {
+            withCredentials: true, // Для использования куки
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Ошибка при получении данных пользователя:", error);
+        throw error;
+    }
+}
+
 const UserHeader: React.FC<UserHeaderProps> = ({ backgroundImage, avatarImage }) => {
-    const { currentUser, isAuth } = useSelector((state: RootState) => state.user);
+    const [user, setUser] = useState<UserData | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const loadUserData = async () => {
+            try {
+                setLoading(true);
+                const data = await fetchUserData();
+                setUser(data); // Устанавливаем данные пользователя
+            } catch (error) {
+                console.error("Не удалось загрузить данные пользователя.");
+            } finally {
+                setLoading(false); // Устанавливаем флаг загрузки в false
+            }
+        };
+
+        loadUserData();
+    }, []);
+
+    if (loading) {
+        return <p>Загрузка...</p>; // Показываем сообщение о загрузке
+    }
 
     return (
         <div
@@ -24,12 +62,12 @@ const UserHeader: React.FC<UserHeaderProps> = ({ backgroundImage, avatarImage })
                 />
             </div>
             <div className="user-info">
-                <h3>Имя: {`${currentUser || "Неизвестно"}`}</h3>
-                <p>Email: {isAuth ? "Электронная почта доступна" : "Электронная почта недоступна"}</p>
+                <h3>{user?.name || "Неизвестно"}</h3>
+                <p>{user?.login || "Неизвестно"}</p>
                 <div className="icons">
-                    <span>⭐</span>
-                    <span>🔊</span>
-                    <span>👥</span>
+                    {/*<span>⭐</span>*/}
+                    {/*<span>🔊</span>*/}
+                    {/*<span>👥</span>*/}
                 </div>
             </div>
         </div>
