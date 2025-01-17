672a7054c1780c91ffc3b2fbc1f9f7df0094dcc2
Айдар <farhutdinov04@gmail.com>
2025-01-18 00:34:29 +0300

    Task84: made squash

--- frontend/src/components/AudioPlayer/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./AudioPlayer.scss"
 import {useAudioPlayer} from "../../hooks/useAudioPlayer";
+import {setDuration} from "../../reducers/audioSlice";
+import {useDispatch} from "react-redux";
 
 export const AudioPlayer = () => {
     const {
         audio,
         tracks,
-        currentTrackIndex,
+        background,
+        trackName,
+        trackAuthor,
         isPlaying,
         currentTime,
         volume,
@@ -15,8 +19,26 @@ export const AudioPlayer = () => {
         handlePrevious,
         handleVolumeChange,
     } = useAudioPlayer();
-
     const [show, setShow] = useState(false);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const updateDuration = () => {
+            if (audio.current) {
+                const trackDuration = audio.current.duration;
+                if (!isNaN(trackDuration)) {
+                    dispatch(setDuration(trackDuration));
+                }
+            }
+        };
+
+        if (audio.current) {
+            audio.current.addEventListener("loadedmetadata", updateDuration);
+            return () => {
+                audio.current?.removeEventListener("loadedmetadata", updateDuration);
+            };
+        }
+    }, []);
 
     return (
         <div className="footer-player">
@@ -24,30 +46,30 @@ export const AudioPlayer = () => {
                 {tracks.length > 0 && (
                     <div className="track-cover">
                         <img
-                            src={JSON.parse(tracks[currentTrackIndex].tag).background}
+                            src={background}
                             alt="Track Cover"
                             className="cover-image"
                         />
                     </div>
                 )}
-
                 <div className="track-name">
-                    <p><b>{tracks.length > 0 && tracks[currentTrackIndex].name}</b></p>
-                    <p>{tracks.length > 0 && tracks[currentTrackIndex].author}</p>
+                    <p><b>{tracks.length > 0 && trackName}</b></p>
+                    <p>{tracks.length > 0 && trackAuthor}</p>
                 </div>
             </div>
             <div className="btn-group gap-2 d-md-block">
-            <button className="btn" onClick={handlePrevious}>
-                    <img className="footer-icon-img" src="/png/Back.png" alt="Previous"/>
+                <button className="btn" onClick={handlePrevious}>
+                    <img className="footer-icon-img" src="../png/Back.png" alt="Previous"/>
                 </button>
                 <button
                     className="btn"
                     onClick={handlePlayPause}
                 >
                     {isPlaying ? (
-                        <img className="footer-icon-img" src="/png/Pause.png" alt="Pause"/>) : currentTime === 0 ? (
-                        <img className="footer-icon-img" src="/png/Play.png" alt="Play"/>) : (
-                        <img className="footer-icon-img" src="/png/Play.png" alt="Play"/>)}
+                        <img className="footer-icon-img" src="../png/Pause.png" alt="Pause"/>) : currentTime === 0 ? (
+                        <img className="footer-icon-img" src="../png/Play.png" alt="Play"/>) : (
+                        <img className="footer-icon-img" src="../png/Play.png" alt="Play"/>)
+                    }
                 </button>
                 <button className="btn " onClick={handleNext}>
                     <img className="footer-icon-img" src="../png/Next.png" alt="Next"/>
@@ -56,20 +78,22 @@ export const AudioPlayer = () => {
                     <img className="footer-icon-img"
                          src="png/Volume.png" alt=""/>
                 </button>
-                {show && <div className="volume-range">
-                    <div className="wrapper"><input
-                        id="volume"
-                        type="range"
-                        min="0"
-                        max="1"
-                        step="0.01"
-                        value={volume}
-                        onChange={handleVolumeChange}
-                    /></div>
-                </div>}
+                {show &&
+                    <div className="volume-range">
+                        <div className="wrapper">
+                            <input
+                                id="volume"
+                                type="range"
+                                min="0"
+                                max="1"
+                                step="0.01"
+                                value={volume}
+                                onChange={handleVolumeChange}
+                            />
+                        </div>
+                    </div>}
                 <audio ref={audio}/>
             </div>
         </div>
     );
 }
-
